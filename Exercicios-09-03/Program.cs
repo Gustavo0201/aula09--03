4e4681775e809e70b2861b54b07935929135470e
Luiz <usher_luiz@hotmail.com>
2022-03-09 22:15:29 -0300

    Aula 09-03

---
+++ Exercicios-09-03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios_09_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Exercício07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercício07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite o seu nome:");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a sua idade:");
+                int idade = int.Parse(Console.ReadLine());
+
+                if (idade >= 0 && idade <= 19)
+                {
+                    Console.WriteLine("Você é jovem!");
+                }
+                else if (idade >= 20 && idade <= 59)
+                {
+                    Console.WriteLine("Você é adulto!");
+                }
+                else if (idade >= 60)
+                {
+                    Console.WriteLine("Você é idoso!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu um erro: { e.Message}");
+            }
+        }
+    }
+}
